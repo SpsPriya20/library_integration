cb1b7186c8dbde2298d29a9dc819b720a2f55f26
priya <swarnapriyasahu@gmail.com>
2018-07-31 20:22:15 +0530

    libraryintegration

---
+++ app/src/main/java/com/priya/libraryintegrationexample/smiley_example_activity.java
@@ -0,0 +1,13 @@
+package com.priya.libraryintegrationexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class smiley_example_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smiley_example_activity);
+    }
+}
